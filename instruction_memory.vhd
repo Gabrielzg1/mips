eb44962acab4a99d30df434a4fff967484f8fc0b
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-11-27 15:18:32 -0300

    .

--- instruction_memory.vhd
+++ instruction_memory.vhd
@@ -13,30 +13,24 @@ end instruction_memory;
 architecture behavioral of instruction_memory is
 
     type mem_array is array(0 to 63) of STD_LOGIC_VECTOR (31 downto 0);
-    signal data_mem: mem_array := ( 
-         -- branch para o valor 5  -- 0
-		  
-		  "00100000000000010000000000000001", -- r1 = 1 
-		  "00010000000000000000000000000100",  -- branch para 16 
-	     "10101100000000010000000000000001", --  mem [ 1 ]           -- 8
-		  
-		  "00100000000000100000000000000001", -- valor fibbo ------ 1 -- 12
-	     "10101100000000100000000000000010", --   mem [ 2 ]          -- 16
-		  
-	     "00100000000000110000000000000010", -- valor fibbo ------ 2 --- 20
-        "10101100000000110000000000000011", --  mem [ 3 ]           -- 24
-		 
-		  "00100000000001000000000000000011", -- valor fibbo ------ 3 -- 28
-		  "10101100000001000000000000000100", --  mem [ 4 ]           -- 32
-		  
-		  "00100000000001010000000000000101", -- valor fibbo ------ 5  -- 36
-	     "10101100000001010000000000000101", --  mem [ 5 ]
-		  
-		  "00100000000001100000000000001000", -- valor fibbo ------ 8
-	     "10101100000001100000000000000110", --  mem [ 6 ]
-		  
-		  "00100000000001110000000000001101", -- valor fibbo ------ 13
-	     "10101100000001110000000000000111", --  mem [ 7 ]
+    signal data_mem: mem_array := ( 		  
+		  "00100000000000010000000000010000",  -- addi $t1, $0, 16       # Armazena 16 em $t0 para contar 16 números de Fibonacci
+		  "00100000000000110000000000000001",  -- addi $t3, $0, 1        # Segundo número de Fibonacci (F1 = 1) o primeiro é 0 e ja é valor padrao 
+		  "10101100100000100000000000000000",  -- sw $t2, 0($t4)         # Armazena F0 no início do array
+		  "00100000100001000000000000000100",  -- addi $t4, $t4, 4       # Incrementa o endereço do array 
+		  "10101100100000110000000000000000",  -- sw $t3, 0($t4)         # Armazena F1 no array 
+		  "00100000100001000000000000000100",  -- addi $t4, $t4, 4       # Incrementa o endereço do array 
+        "00100000001000011111111111111110",	-- addi $t1, $t1, -2      # Decrementa o contador em 2 (já que já armazenamos 2 números)
+																
+														   -- fib_loop: 
+		  "00010000000000010000000000010000",  -- beq $t1, $0, end       # Se o contador é zero então termina	 -- TROCAR O ENDERECO DE FIM 
+		  "00000000010000110010100000100000",  -- add $t5, $t2, $t3      # Calcula o próximo número de Fibonacci (F(n) = F(n-1) + F(n-2))
+		  "10101100100001010000000000000000",  -- sw $t5, 0($t4)         # Armazena o novo número de Fibonacci no array
+		  "00000000011000000001000000100000",  -- add $t2, $t3, $zero    # Move F(n-1) para F(n-2) para a próxima iteração
+		  "00000000101000000001100000100000",  -- add $t3, $t5, $zero    # Move F(n) para F(n-1) para a próxima iteração
+		  "00100000100001000000000000000100",  -- addi $t4, $t4, 4       # Incrementa o endereço do array 
+		  "00100000001000011111111111111111",  -- addi $t1, $t1, -1      # Decrementa o contador
+		  "00001000000000000000000000001000",	-- j fib_loop             # Volta ao início do loop
 		  
 		  
         -- ... mais instruções ...
